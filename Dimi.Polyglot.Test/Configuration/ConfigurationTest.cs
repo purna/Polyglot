020f565475c20ffec650ffe6eefa4d2c5e60447d
Dimitrios Kourkoulis <developer@dimitros.net>
2013-08-17 19:35:36 +0200

    Started implementing non-standard cultures and introduced NUnit to the project

---
+++ Dimi.Polyglot.Test/Configuration/ConfigurationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Dimi.Polyglot.Test.Configuration
+{
+    [TestFixture]
+    public class ConfigurationTest
+    {
+        [Test]
+        public void LoadConfiguration()
+        {
+            var nonStandardCultures = Dimi.Polyglot.Configuration.Configuration.NonStandardCultures;
+            Assert.True(nonStandardCultures.Count == 2);
+            Assert.True(nonStandardCultures.First().ISOCode == "n1");
+            Assert.True(nonStandardCultures.First().Description == "Non-standard 1");
+            Assert.True(nonStandardCultures.First().CultureAlias == "Non-standard alias 1");
+            Assert.True(nonStandardCultures.Last().ISOCode == "n2");
+            Assert.True(nonStandardCultures.Last().Description == "Non-standard 2");
+            Assert.True(nonStandardCultures.Last().CultureAlias == "Non-standard alias 2");
+        }
+    }
+}
---
+++ Dimi.Polyglot/Configuration/Configuration.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Dimi.Polyglot.Model;
+
+namespace Dimi.Polyglot.Configuration
+{
+    /// <summary>
+    /// Configuration for Polyglot
+    /// </summary>
+    public static class Configuration
+    {
+        private static readonly object ThreadSafetyLock = new object();
+        private static XDocument _document;
+        private static IList<Language> _nonStandardCultures;
+
+        /// <summary>
+        /// Loads the XML configuration document.
+        /// </summary>
+        private static void LoadDocument()
+        {
+            if (_document == null)
+            {
+                lock (ThreadSafetyLock)
+                {
+                    // Load document with mappath if on the web. Otherwise use direct
+                    // path, since we're probably unit testing.
+                    _document = XDocument.Load(System.Web.Hosting.HostingEnvironment.IsHosted ?
+                         System.Web.Hosting.HostingEnvironment.MapPath("/config/Polyglot.config") :
+                        "..\\..\\..\\Dimi.Polyglot\\config\\PolyglotTest.config");
+                }
+                LoadNonStandardCultures();
+            }
+        }
+
+        /// <summary>
+        /// Loads the non-standard cultures from the configuration file.
+        /// </summary>
+        private static void LoadNonStandardCultures()
+        {
+            _nonStandardCultures = new List<Language>();
+            var elements = _document.XPathSelectElements("/PolyglotConfiguration/nonStandardCultures/culture");
+            foreach (var element in elements)
+            {
+                var isoCode = element.Attribute("ISOCode");
+                var description = element.Attribute("Description");
+                var cultureAlias = element.Attribute("CultureAlias");
+                _nonStandardCultures.Add(new Language()
+                    {
+                        ISOCode = isoCode.Value,
+                        Description = description.Value,
+                        CultureAlias = cultureAlias.Value
+                    });
+
+            }
+        }
+
+        public static IList<Language> NonStandardCultures
+        {
+            get
+            {
+                LoadDocument();
+                return _nonStandardCultures;
+            }
+        }
+
+
+    }
+}
