6040ad516d1ab31a96bc7f850b59265c8701dc07
Dimitri Kourkoulis <developer@dimitros.be>
2014-03-12 21:13:53 +0100

    Correction for crash when creating nodes from the root

--- Dimi.Polyglot/Web/Backoffice/MenuCustomisation.cs
+++ Dimi.Polyglot/Web/Backoffice/MenuCustomisation.cs
@@ -14,13 +14,19 @@ namespace Dimi.Polyglot.Web.Backoffice
 
         void ContentTreeController_MenuRendering(Umbraco.Web.Trees.TreeControllerBase sender, Umbraco.Web.Trees.MenuRenderingEventArgs e)
         {
-
-            if (!string.IsNullOrEmpty(sender.TreeAlias) && sender.TreeAlias.ToLower() == "content")
+            try
             {
-                var i = new Umbraco.Web.Models.Trees.MenuItem("polyglotCreateTranslations", "[Polyglot] Create translations");
-                i.AdditionalData.Add("actionUrl", "/Umbraco/Plugins/Dimi.Polyglot/TranslationCreation.aspx?NodeId=" + e.NodeId);
-                i.Icon = "chat";
-                e.Menu.Items.Insert(12, i);
+                if (!string.IsNullOrEmpty(sender.TreeAlias) && sender.TreeAlias.ToLower() == "content")
+                {
+                    var i = new Umbraco.Web.Models.Trees.MenuItem("polyglotCreateTranslations", "[Polyglot] Create translations");
+                    i.AdditionalData.Add("actionUrl", "/Umbraco/Plugins/Dimi.Polyglot/TranslationCreation.aspx?NodeId=" + e.NodeId);
+                    i.Icon = "chat";
+                    e.Menu.Items.Insert(12, i);
+                }
+            }
+            catch
+            {
+                // Do not cause a problem in Umbraco if node information is not provided.
             }
         }
     }
